2c7f68179c8e71792e9fd7aea91060df9b5b38fa
FlashyDJ <diamond_john@outlook.ph>
2023-09-29 15:56:27 +0800

    Add new unit tests for SlotMapICollectionTests

--- tests/Slotmaps.Tests/SlotMapICollectionTests.cs
+++ tests/Slotmaps.Tests/SlotMapICollectionTests.cs
@@ -1,4 +1,4 @@
-﻿using ISlotCollection = System.Collections.Generic.ICollection<System.Collections.Generic.KeyValuePair<FlashyDJ.Slotmaps.SlotKey, string>>;
+﻿using ISlotCollection = System.Collections.Generic.ICollection<System.Collections.Generic.KeyValuePair<FlashyDJ.Slotmaps.SlotKey, int>>;
 
 namespace Slotmaps.Tests;
 public class SlotMapICollectionTests
@@ -6,126 +6,357 @@ public class SlotMapICollectionTests
     public class Add
     {
         [Fact]
-        public void AddsKeyValuePairToSlotMap_ShouldContainAddedKeyAndValue()
+        public void ValidKeyAndValue_AddsKeyValuePairToMap()
         {
-            ISlotCollection iSlotMap = new SlotMap<string>(1);
-            var key0 = new SlotKey(0, 0);
+            var slotMap = new SlotMap<int>();
+            var key = slotMap.Add(24);
+            var kvp = new KeyValuePair<SlotKey, int>(key, 42);
 
-            iSlotMap.Add(new(key0, "TestValue"));
+            (slotMap as ISlotCollection).Add(kvp);
 
-            Assert.True(iSlotMap.Contains(new(key0 with { Version = 1}, "TestValue")));
+            Assert.Equal(42, slotMap[key with { Version = 3 }]);
+        }
+
+        [Fact]
+        public void InvalidKey_ThrowsKeyNotFoundException()
+        {
+            var slotMap = new SlotMap<int>();
+            var invalidKey = new SlotKey(0, -1);
+            var kvp = new KeyValuePair<SlotKey, int>(invalidKey, 42);
+
+            var ex = Assert.Throws<KeyNotFoundException>(() => (slotMap as ISlotCollection).Add(kvp));
+            Assert.Equal("Invalid SlotKey", ex.Message);
+        }
+
+        [Fact]
+        public void OlderVersionKey_ThrowsKeyNotFoundException()
+        {
+            var slotMap = new SlotMap<int>();
+            var key1 = slotMap.Add(42);
+            var key2 = key1 with { Version = 0 };
+            var kvp = new KeyValuePair<SlotKey, int>(key2, 24);
+
+            var ex = Assert.Throws<KeyNotFoundException>(() => (slotMap as ISlotCollection).Add(kvp));
+            Assert.Equal("Invalid SlotKey", ex.Message);
         }
     }
     
     public class Clear
     {
         [Fact]
-        public void WithElementsAdded_ShouldRemoveAllElementsFromSlotMap()
+        public void WithItems_ClearsMapAndSetsCountToZero()
         {
-            ISlotCollection iSlotMap = new SlotMap<string>(1);
-            iSlotMap.Add(new(new(0,0), "TestValue"));
+            var slotMap = new SlotMap<int>(2);
+            slotMap.Add(42);
+            slotMap.Add(24);
 
-            iSlotMap.Clear();
+            (slotMap as ISlotCollection).Clear();
 
-            Assert.Equal(0, iSlotMap.Count);
+            Assert.Equal(0, (slotMap as ISlotCollection).Count);
+            Assert.Empty((slotMap as ISlotCollection));
         }
     }
     
     public class Contains
     {
         [Fact]
-        public void ExistingKeyValuePair_ShouldReturnTrue()
+        public void ExistingKeyValuePair_ReturnsTrue()
         {
-            SlotMap<string> slotMap = new SlotMap<string>();
-            SlotKey key = slotMap.Add("TestValue");
+            var slotMap = new SlotMap<int>();
+            var key = slotMap.Add(42);
+            var kvp = new KeyValuePair<SlotKey, int>(key, 42);
 
-            ISlotCollection iSlotMap = slotMap;
-            bool containsKey = iSlotMap.Contains(new(key, "TestValue"));
+            bool containsKey = (slotMap as ISlotCollection).Contains(kvp);
 
             Assert.True(containsKey);
-            Assert.True(slotMap.ContainsKey(key));
         }
 
         [Fact]
-        public void KeyValuePairNotPresent_ShouldReturnFalse()
+        public void NonexistentKeyValuePair_ReturnsFalse()
         {
-            SlotMap<string> slotMap = new SlotMap<string>();
-            ISlotCollection iSlotMap = slotMap;
+            var slotMap = new SlotMap<int>();
+            var key = new SlotKey(1, 1);
+            var kvp = new KeyValuePair<SlotKey, int>(key, 42);
 
-            bool containsKey = iSlotMap.Contains(new(SlotKey.Null, ""));
+            bool containsKey = (slotMap as ISlotCollection).Contains(kvp);
 
             Assert.False(containsKey);
+        }
+
+        [Fact]
+        public void InvalidKey_ReturnsFalse()
+        {
+            var slotMap = new SlotMap<int>();
+            var invalidKey = new SlotKey(0, -1);
+            var kvp = new KeyValuePair<SlotKey, int>(invalidKey, 42);
+
+            var result = (slotMap as ISlotCollection).Contains(kvp);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void KeyWithOlderVersion_ReturnsFalse()
+        {
+            var slotMap = new SlotMap<int>();
+            var key1 = slotMap.Add(42);
+            var key2 = key1 with { Version = 0 };
+            var kvp = new KeyValuePair<SlotKey, int>(key2, 24);
+
+            var result = (slotMap as ISlotCollection).Contains(kvp);
+
+            Assert.False(result);
         }
     }
     
     public class CopyTo
     {
         [Fact]
-        public void ElementsAddedToSlotMap_CopyToArray()
+        public void ValidArguments_CopiesKeyValuePairsToArray()
         {
-            SlotMap<string> slotMap = new SlotMap<string>(3);
-            slotMap.Add( "One");
-            slotMap.Add( "Two");
-            slotMap.Add( "Three");
+            var slotMap = new SlotMap<int>();
+            var key1 = slotMap.Add(42);
+            var key2 = slotMap.Add(24);
+            var key3 = slotMap.Add(36);
 
-            KeyValuePair<SlotKey, string>[] array = new KeyValuePair<SlotKey, string>[3];
-            ISlotCollection iSlotMap = slotMap;
+            var array = new KeyValuePair<SlotKey, int>[3];
 
-            iSlotMap.CopyTo(array, 0);
+            (slotMap as ISlotCollection).CopyTo(array, 0);
 
-            Assert.Equal(new KeyValuePair<SlotKey, string>(new SlotKey(0,1), "One"), array[0]);
-            Assert.Equal(new KeyValuePair<SlotKey, string>(new SlotKey(1,1), "Two"), array[1]);
-            Assert.Equal(new KeyValuePair<SlotKey, string>(new SlotKey(2,1), "Three"), array[2]);
+            Assert.Equal(new KeyValuePair<SlotKey, int>(key1, 42), array[0]);
+            Assert.Equal(new KeyValuePair<SlotKey, int>(key2, 24), array[1]);
+            Assert.Equal(new KeyValuePair<SlotKey, int>(key3, 36), array[2]);
+        }
+
+        [Fact]
+        public void InvalidArray_ThrowsArgumentNullException()
+        {
+            var slotMap = new SlotMap<int>();
+            var array = (KeyValuePair<SlotKey, int>[])null!;
+
+            Assert.Throws<ArgumentNullException>(
+                () => (slotMap as ISlotCollection).CopyTo(array, 0)
+            );
+        }
+
+        [Fact]
+        public void NegativeIndex_ThrowsArgumentOutOfRangeException()
+        {
+            var slotMap = new SlotMap<int>();
+            var array = new KeyValuePair<SlotKey, int>[1];
+
+            Assert.Throws<ArgumentOutOfRangeException>(
+                () => (slotMap as ISlotCollection).CopyTo(array, -1)
+            );
+        }
+
+        [Fact]
+        public void IndexGreaterThanArrayLength_ThrowsArgumentOutOfRangeException()
+        {
+            var slotMap = new SlotMap<int>();
+            var array = new KeyValuePair<SlotKey, int>[1];
+
+            Assert.Throws<ArgumentOutOfRangeException>(
+                () => (slotMap as ISlotCollection).CopyTo(array, 2)
+            );
+        }
+
+        [Fact]
+        public void NotEnoughSpaceInArray_ThrowsArgumentOutOfRangeException()
+        {
+            var slotMap = new SlotMap<int>();
+            var array = new KeyValuePair<SlotKey, int>[2];
+
+            Assert.Throws<ArgumentOutOfRangeException>(
+                () => (slotMap as ISlotCollection).CopyTo(array, 1)
+            );
         }
     }
     
     public class Enumerator
     {
         [Fact]
-        public void EnumeratingItemsInSlotMap_ShouldEnumerateCorrectly()
+        public void GetEnumerator_EnumeratesAllKeyValuePairs()
         {
-            SlotMap<string> slotMap = new SlotMap<string>(3);
-            ISlotCollection iSlotMap = slotMap;
+            var slotMap = new SlotMap<int>();
+            var key1 = slotMap.Add(42);
+            var key2 = slotMap.Add(24);
 
-            slotMap.Add("10");
-            slotMap.Add("20");
-            slotMap.Add("30");
-
-            var enumerator = iSlotMap.GetEnumerator();
-            var enumeratedItems = new List<KeyValuePair<SlotKey, string>>();
-            foreach (var item in iSlotMap)
+            var expectedPairs = new List<KeyValuePair<SlotKey, int>>
             {
-                enumeratedItems.Add(item);
+                new KeyValuePair<SlotKey, int>(key1, 42),
+                new KeyValuePair<SlotKey, int>(key2, 24)
+            };
+
+            var enumerator = (slotMap as ISlotCollection).GetEnumerator();
+            var actualPairs = new List<KeyValuePair<SlotKey, int>>();
+            while (enumerator.MoveNext())
+            {
+                actualPairs.Add(enumerator.Current);
             }
-            // while (enumerator.MoveNext())
-            // {
-            //     enumeratedItems.Add(enumerator.Current);
-            // }
+
+            Assert.Equal(expectedPairs, actualPairs);
+        }
+
+        [Fact]
+        public void GetEnumerator_EmptyMap_ReturnsEmptyEnumerator()
+        {
+            var slotMap = new SlotMap<int>();
+
+            var enumerator = (slotMap as ISlotCollection).GetEnumerator();
+
+            Assert.False(enumerator.MoveNext());
+        }
+
+        [Fact]
+        public void GetEnumerator_AfterRemovingKey_EnumeratesRemainingKeyValuePairs()
+        {
+            var slotMap = new SlotMap<int>();
+            var key1 = slotMap.Add(42);
+            var key2 = slotMap.Add(24);
+
+            var expectedPairs = new List<KeyValuePair<SlotKey, int>>
+            {
+                new KeyValuePair<SlotKey, int>(key2, 24)
+            };
+
+            slotMap.Remove(key1);
+
+            var enumerator = (slotMap as ISlotCollection).GetEnumerator();
+            var actualPairs = new List<KeyValuePair<SlotKey, int>>();
+            while (enumerator.MoveNext())
+            {
+                actualPairs.Add(enumerator.Current);
+            }
+
+            Assert.Equal(expectedPairs, actualPairs);
+        }
+
+        [Fact]
+        public void Enumerator_ShouldEnumerateCorrectly()
+        {
+            var slotMap = new SlotMap<int>(3);
+
+            var key1 = slotMap.Add(10);
+            var key2 = slotMap.Add(20);
+            var key3 = slotMap.Add(30);
+
+            var enumerator = (slotMap as ISlotCollection).GetEnumerator();
+            var enumeratedItems = new List<KeyValuePair<SlotKey, int>>();
+
+            while (enumerator.MoveNext())
+            {
+                enumeratedItems.Add(enumerator.Current);
+            }
 
             Assert.Equal(3, enumeratedItems.Count);
 
-            Assert.Contains(new KeyValuePair<SlotKey, string>(new SlotKey(0,1), "10"), enumeratedItems);
-            Assert.Contains(new KeyValuePair<SlotKey, string>(new SlotKey(1,1), "20"), enumeratedItems);
-            Assert.Contains(new KeyValuePair<SlotKey, string>(new SlotKey(2,1), "30"), enumeratedItems);
+            Assert.Contains(new KeyValuePair<SlotKey, int>(key1, 10), enumeratedItems);
+            Assert.Contains(new KeyValuePair<SlotKey, int>(key2, 20), enumeratedItems);
+            Assert.Contains(new KeyValuePair<SlotKey, int>(key3, 30), enumeratedItems);
         }
     }
-    
+
+    public class ForEachLoop
+    {
+        [Fact]
+        public void EnumeratesAllKeyValuePairs()
+        {
+            var slotMap = new SlotMap<int>();
+            var key1 = slotMap.Add(42);
+            var key2 = slotMap.Add(24);
+
+            var expectedPairs = new List<KeyValuePair<SlotKey, int>>
+            {
+                new KeyValuePair<SlotKey, int>(key1, 42),
+                new KeyValuePair<SlotKey, int>(key2, 24)
+            };
+
+            var actualPairs = new List<KeyValuePair<SlotKey, int>>();
+            foreach (var kvp in slotMap)
+            {
+                actualPairs.Add(kvp);
+            }
+
+            Assert.Equal(expectedPairs, actualPairs);
+        }
+
+        [Fact]
+        public void EmptyMap_DoesNotEnterLoop()
+        {
+            var slotMap = new SlotMap<int>();
+            bool enteredLoop = false;
+
+            foreach (var kvp in slotMap)
+            {
+                enteredLoop = true;
+            }
+
+            Assert.False(enteredLoop);
+        }
+
+        [Fact]
+        public void AfterRemovingKey_EnumeratesRemainingKeyValuePairs()
+        {
+            var slotMap = new SlotMap<int>();
+            var key1 = slotMap.Add(42);
+            var key2 = slotMap.Add(24);
+
+            slotMap.Remove(key1);
+
+            var expectedPairs = new List<KeyValuePair<SlotKey, int>>
+            {
+                new KeyValuePair<SlotKey, int>(key2, 24)
+            };
+
+            var actualPairs = new List<KeyValuePair<SlotKey, int>>();
+            foreach (var kvp in slotMap)
+            {
+                actualPairs.Add(kvp);
+            }
+
+            Assert.Equal(expectedPairs, actualPairs);
+        }
+    }
+
     public class Remove
     {
         [Fact]
-        public void RemoveKeyValuePairFromSlotMap_ShouldSucceed()
+        public void ExistingKeyValuePair_RemovesKeyValuePairAndReturnsTrue()
         {
-            ISlotCollection iSlotMap = new SlotMap<string>(1);
-            var key0 = new SlotKey(0, 0);
+            var slotMap = new SlotMap<int>();
+            var key = slotMap.Add(42);
+            var kvp = new KeyValuePair<SlotKey, int>(key, 42);
 
-            iSlotMap.Add(new(key0, "TestValue"));
+            var result = (slotMap as ISlotCollection).Remove(kvp);
 
-            bool removed = iSlotMap.Remove(new(key0 with { Version = 1 }, "TestValue"));
-
-            Assert.True(removed);
-            Assert.False(iSlotMap.Contains(new(key0 with { Version = 1 }, "TestValue")));
+            Assert.True(result);
+            Assert.Empty(slotMap);
         }
 
+        [Fact]
+        public void NonexistentKeyValuePair_ReturnsFalse()
+        {
+            var slotMap = new SlotMap<int>();
+            var key = new SlotKey(1, 1);
+            var kvp = new KeyValuePair<SlotKey, int>(key, 42);
+
+            var result = (slotMap as ISlotCollection).Remove(kvp);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void InvalidKey_ThrowsKeyNotFoundException()
+        {
+            var slotMap = new SlotMap<int>();
+            var invalidKey = new SlotKey(0, -1);
+            var kvp = new KeyValuePair<SlotKey, int>(invalidKey, 42);
+
+            var result = (slotMap as ISlotCollection).Remove(kvp);
+
+            Assert.False(result);
+        }
     }
 }
 
