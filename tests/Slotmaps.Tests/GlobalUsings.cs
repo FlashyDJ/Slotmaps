d396691ba3bd53801b21b1f589d4f2ae1038beb0
FlashyDJ <diamond_john@outlook.ph>
2023-10-05 12:10:33 +0800

    Clean gh-pages branch for docfx

--- tests/Slotmaps.Tests/GlobalUsings.cs
+++
@@ -1,6 +0,0 @@
-global using global::System;
-global using global::System.Collections;
-global using global::System.Collections.Generic;
-global using global::System.Linq;
-global using FlashyDJ.Slotmaps;
-global using Xunit;
