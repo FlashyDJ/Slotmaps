8f7db76b760f9abee7930bac0c8b34aa7461dbe0
FlashyDJ <diamond_john@outlook.ph>
2023-10-08 13:54:13 +0800

    Add System.Linq

--- src/Slotmaps/Usings.cs
+++ src/Slotmaps/Usings.cs
@@ -3,3 +3,4 @@ global using global::System.Collections;
 global using global::System.Collections.Generic;
 global using global::System.Diagnostics;
 global using global::System.Diagnostics.CodeAnalysis;
+global using global::System.Linq;
