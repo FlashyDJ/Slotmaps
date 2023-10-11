aa1f875c464d582f3183e9d0c85db7101f2ce4e4
FlashyDJ <diamond_john@outlook.ph>
2023-10-11 17:22:35 +0800

    Use int instead of uint for Index

--- src/Slotmaps/ISlotKey.cs
+++ src/Slotmaps/ISlotKey.cs
@@ -1,20 +1,22 @@
 public interface ISlotKey<TSelf> where TSelf : struct
 {
-    uint Index { get; }
+    int Index { get; }
     uint Version { get; }
+    bool IsNull => Index > 0;
     bool Occupied => Version % 2 > 0;
-    bool IsNull => Index == uint.MaxValue;
 
-    abstract static TSelf New(uint index, uint version);
+    abstract static TSelf New(int index, uint version);
 
     virtual static TSlotKey Null<TSlotKey>()
         where TSlotKey : struct, ISlotKey<TSlotKey> =>
-        TSlotKey.New(uint.MaxValue, 1);
+        TSlotKey.New(int.MinValue, 1);
 
     virtual static ulong ToUlong(ISlotKey<TSelf> slotKey) =>
-        ((ulong)slotKey.Version << 32) | slotKey.Index;
+        ((ulong)slotKey.Version << 32) | (uint)slotKey.Index;
 
     virtual static TSlotKey FromUlong<TSlotKey>(ulong value)
         where TSlotKey : struct, ISlotKey<TSlotKey> =>
-        TSlotKey.New((uint)value, ((uint)value >> 32) | 1);
+        TSlotKey.New((int)value, ((uint)value >> 32) | 1);
+
+    string ToString() => $"{Index}v{Version}";
 }
--- src/Slotmaps/SlotKey.cs
+++ src/Slotmaps/SlotKey.cs
@@ -2,32 +2,32 @@
 #pragma warning disable CS1572 // XML comment has a param tag, but there is no parameter by that name (there is and it works)
 /// <include file='docs.xml' path='docs/SlotKey/*'/>
 #pragma warning restore CS1572 // XML comment has a param tag, but there is no parameter by that name
-public readonly record struct SlotKey(uint Index, uint Version) : ISlotKey<SlotKey>
+public readonly record struct SlotKey(int Index, uint Version) : ISlotKey<SlotKey>
 {
-    public static SlotKey New(uint index, uint version) => new(index, version);
+    /// <include file='docs.xml' path='docs/IsNull/*'/>
+    public bool IsNull => Index > 0;
 
     public bool Occupied => Version % 2 > 0;
 
-    /// <include file='docs.xml' path='docs/IsNull/*'/>
-    public bool IsNull => Index == uint.MaxValue;
+    public static SlotKey New(int index, uint version) => new(index, version);
 
     /// <include file='docs.xml' path='docs/Null/*'/>
-    public static SlotKey Null() => new(uint.MinValue, 0);
+    public static SlotKey Null() => new(int.MinValue, 0);
 
     public static ulong ToUlong(ISlotKey<SlotKey> slotKey) =>
-        ((ulong)slotKey.Version << 32) | slotKey.Index;
+        ((ulong)slotKey.Version << 32) | (uint)slotKey.Index;
 
     public static SlotKey FromUlong<SlotKey>(ulong value)
         where SlotKey : struct, ISlotKey<SlotKey> =>
-        SlotKey.New((uint)value, ((uint)value >> 32) | 1);
+        SlotKey.New((int)value, ((uint)value >> 32) | 1);
 
     /// <include file='docs.xml' path='docs/SlotKeyToUlong/*'/>
     public static implicit operator ulong(SlotKey slotKey) =>
-        ((ulong)slotKey.Version << 32) | slotKey.Index;
+        ((ulong)slotKey.Version << 32) | (uint)slotKey.Index;
 
     /// <include file='docs.xml' path='docs/UlongToSlotKey/*'/>
     public static implicit operator SlotKey(ulong value) =>
-        new((uint)value, ((uint)value >> 32) | 1);
+        new((int)value, ((uint)value >> 32) | 1);
 
     /// <include file='docs.xml' path='docs/ToString/*'/>
     public override string ToString() => $"{Index}v{Version}";
