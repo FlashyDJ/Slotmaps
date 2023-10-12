4a0d50b35b1d6498ab8413039a45227a789fce6a
FlashyDJ <diamond_john@outlook.ph>
2023-10-12 13:16:43 +0800

    Update XML Documentation for SlotKey

--- src/Slotmaps/SlotKey.cs
+++ src/Slotmaps/SlotKey.cs
@@ -1,34 +1,38 @@
 ﻿namespace FlashyDJ.Slotmaps;
-#pragma warning disable CS1572 // XML comment has a param tag, but there is no parameter by that name (there is and it works)
 /// <include file='docs.xml' path='docs/SlotKey/*'/>
-#pragma warning restore CS1572 // XML comment has a param tag, but there is no parameter by that name
+/// <param name="Index">The index of the slot.</param>
+/// <param name = "Version" > The version of the slot.</param>
 public readonly record struct SlotKey(int Index, uint Version) : ISlotKey<SlotKey>
 {
-    /// <include file='docs.xml' path='docs/IsNull/*'/>
+    /// <inheritdoc/>
     public bool IsNull => Index < 0;
 
+    /// <inheritdoc/>
     public bool Occupied => Version % 2 > 0;
 
+    /// <inheritdoc/>
     public static SlotKey New(int index, uint version) => new(index, version);
 
-    /// <include file='docs.xml' path='docs/Null/*'/>
+    /// <inheritdoc/> <include file='docs.xml' path='docs/Null/*'/>
     public static SlotKey Null() => new(int.MinValue, 0);
 
+    /// <inheritdoc/>
     public static ulong ToUlong(ISlotKey<SlotKey> slotKey) =>
         ((ulong)slotKey.Version << 32) | (uint)slotKey.Index;
 
+    /// <inheritdoc/>
     public static SlotKey FromUlong<SlotKey>(ulong value)
         where SlotKey : struct, ISlotKey<SlotKey> =>
         SlotKey.New((int)value, ((uint)value >> 32) | 1);
 
-    /// <include file='docs.xml' path='docs/SlotKeyToUlong/*'/>
+    /// <include file='docs.xml' path='docs/ImplicitToUlong/*'/>
     public static implicit operator ulong(SlotKey slotKey) =>
         ((ulong)slotKey.Version << 32) | (uint)slotKey.Index;
 
-    /// <include file='docs.xml' path='docs/UlongToSlotKey/*'/>
+    /// <include file='docs.xml' path='docs/ImplicitFromUlong/*'/>
     public static implicit operator SlotKey(ulong value) =>
         new((int)value, ((uint)value >> 32) | 1);
 
-    /// <include file='docs.xml' path='docs/ToString/*'/>
+    /// <inheritdoc cref="ISlotKey{TSelf}.ToString"/>
     public override string ToString() => $"{Index}v{Version}";
 }
