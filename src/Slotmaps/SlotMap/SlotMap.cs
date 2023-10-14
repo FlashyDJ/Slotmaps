81dbc0799f5e74b785e7561c1d1f9d456d3ec3a8
FlashyDJ <diamond_john@outlook.ph>
2023-10-15 01:18:02 +0800

    Add example code for default slotmap

--- src/Slotmaps/SlotMap/SlotMap.cs
+++ src/Slotmaps/SlotMap/SlotMap.cs
@@ -31,47 +31,62 @@ public class SlotMap<TValue> : SlotMap<SlotKey, TValue>
     }
 
     /// <include file='docs.xml' path='docs/Add/*'/>
+    /// <include file='codesnippets.xml' path='code/AddD/*'/>
     public new SlotKey Add(TValue value) => base.Add(value);
 
     /// <include file='docs.xml' path='docs/ContainsKey/*'/>
+    /// <include file='codesnippets.xml' path='code/ContainsKeyD/*'/>
     public new bool ContainsKey(SlotKey key) => base.ContainsKey(key);
 
     /// <include file='docs.xml' path='docs/ContainsValue/*'/>
+    /// <include file='codesnippets.xml' path='code/ContainsValueD/*'/>
     public new bool ContainsValue(TValue value) => base.ContainsValue(value);
 
     /// <include file='docs.xml' path='docs/Clear/*'/>
+    /// <include file='codesnippets.xml' path='code/ClearD/*'/>
     public new void Clear() => base.Clear();
 
     /// <include file='docs.xml' path='docs/Drain/*'/>
+    /// <include file='codesnippets.xml' path='docs/DrainD/*'/>
     public new IEnumerable<KeyValuePair<SlotKey,TValue>> Drain() => base.Drain();
 
     /// <include file='docs.xml' path='docs/EnsureCapacity/*'/>
+    /// <include file='codesnippets.xml' path='docs/EnsureCapacityD/*'/>
     public new int EnsureCapacity(int capacity) => base.EnsureCapacity(capacity);
 
     /// <include file='docs.xml' path='docs/Get/*'/>
+    /// <include file='codesnippets.xml' path='docs/GetD/*'/>
     public new TValue Get(SlotKey key) => base.Get(key);
 
     /// <include file='docs.xml' path='docs/Insert/*'/>
+    /// <include file='codesnippets.xml' path='docs/InsertD/*'/>
     public new SlotKey Insert(SlotKey key, TValue value) => base.Insert(key, value);
 
     /// <include file='docs.xml' path='docs/Remove/*'/>
+    /// <include file='codesnippets.xml' path='docs/RemoveD/*'/>
     public new TValue Remove(SlotKey key) => Remove(key);
 
     /// <include file='docs.xml' path='docs/Reserve/*'/>
+    /// <include file='codesnippets.xml' path='docs/ReserveD/*'/>
     public new void Reserve(int additionalSize) => Reserve(additionalSize);
 
     /// <include file='docs.xml' path='docs/Resize/*'/>
+    /// <include file='codesnippets.xml' path='docs/ResizeD/*'/>
     public new void Resize(int newSize) => base.Resize(newSize);
 
     /// <include file='docs.xml' path='docs/Retain/*'/>
+    /// <include file='codesnippets.xml' path='docs/RetainD/*'/>
     public new void Retain(Func<SlotKey,TValue, bool> predicate) => base.Retain(predicate);
 
     /// <include file='docs.xml' path='docs/TryInsert/*'/>
+    /// <include file='codesnippets.xml' path='docs/TryInsertD/*'/>
     public new bool TryInsert(SlotKey key, TValue value, out SlotKey newKey) => base.TryInsert(key, value, out newKey);
 
     /// <include file='docs.xml' path='docs/TryGet/*'/>
+    /// <include file='codesnippets.xml' path='docs/TryGetD/*'/>
     public new bool TryGet(SlotKey key, [MaybeNullWhen(false)] out TValue value) => base.TryGet(key, out value);
 
     /// <include file='docs.xml' path='docs/TryRemove/*'/>
+    /// <include file='codesnippets.xml' path='docs/TryRemoveD/*'/>
     public new bool TryRemove(SlotKey key, [MaybeNullWhen(false)] out TValue value) => base.TryRemove(key, out value);
 }
