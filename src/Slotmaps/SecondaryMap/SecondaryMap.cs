b7d0daac052d21b90ff1bf20df090f6109e08ca0
FlashyDJ <diamond_john@outlook.ph>
2023-10-16 14:01:40 +0800

    Add documentation for secondarymap with slotkey

--- src/Slotmaps/SecondaryMap/SecondaryMap.cs
+++ src/Slotmaps/SecondaryMap/SecondaryMap.cs
@@ -1,8 +1,11 @@
 namespace FlashyDJ.Slotmaps;
+/// <include file='docs.xml' path='docs/SecondaryMapD/*'/>
 [DebuggerDisplay("Count = {Count}")]
 public class SecondaryMap<TValue> : SecondaryMap<SlotKey, TValue>
 {
+    /// <include file='docs.xml' path='docs/Ctor1D/*'/>
     public SecondaryMap() : base() {}
 
+/// <include file='docs.xml' path='docs/Ctor2D/*'/>
     public SecondaryMap(int capacity) : base(capacity) {}
 }
