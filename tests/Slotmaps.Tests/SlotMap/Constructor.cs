030230b2d58f520552be4390ab76a556da98e2e1
FlashyDJ <77375978+FlashyDJ@users.noreply.github.com>
2023-11-18 11:52:24 +0800

    Add PrimaryConstructorTestTemplate

---
+++ tests/Slotmaps.Tests/SlotMap/Constructor.cs
@@ -0,0 +1,20 @@
+//////////////////////////////////////////|WARNING|///////////////////////////////////////////
+//		    			        GENERATED CODE - DO NOT MODIFY      		    	  		//
+//    Changes will not be permanent. Update the T4 template files instead. (*.t4) (*.tt)    //
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+namespace Slotmaps.Tests.SlotMap;
+public class Constructor
+{
+    [Fact]
+    public void WithCapacityParameter_SetsCapacityAndBeEmpty()
+    {
+        int capacity = 10;
+
+        var slotMap = new SlotMap<int>(capacity);
+
+        Assert.Equal(capacity, slotMap.Capacity);
+        Assert.Empty(slotMap);
+        Assert.True(slotMap.IsEmpty);
+    }
+}
