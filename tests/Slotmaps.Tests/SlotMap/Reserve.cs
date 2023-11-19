5c8e9dc7cde6059c6d84b307520ddc157be6752a
FlashyDJ <77375978+FlashyDJ@users.noreply.github.com>
2023-11-19 13:49:57 +0800

    Add PrimaryReserveTestTemplate

---
+++ tests/Slotmaps.Tests/SlotMap/Reserve.cs
@@ -0,0 +1,29 @@
+//////////////////////////////////////////|WARNING|///////////////////////////////////////////
+//		    			        GENERATED CODE - DO NOT MODIFY      		    	  		//
+//    Changes will not be permanent. Update the T4 template files instead. (*.t4) (*.tt)    //
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+namespace Slotmaps.Tests.SlotMap;
+public class Reserve
+{
+    [Fact]
+    public void ValidAdditionalSize_ResizesMap()
+    {
+        var slotMap = new SlotMap<int>();
+        var initialCapacity = slotMap.Capacity;
+        var additionalSize = 10;
+   
+        slotMap.Reserve(additionalSize);
+   
+        Assert.Equal(initialCapacity + additionalSize, slotMap.Capacity);
+    }
+   
+    [Fact]
+    public void Reserve_NegativeAdditionalSize_ThrowsArgumentOutOfRangeException()
+    {
+        var slotMap = new SlotMap<int>();
+        var negativeSize = -10;
+   
+        Assert.Throws<ArgumentOutOfRangeException>(() => slotMap.Reserve(negativeSize));
+    }
+}
