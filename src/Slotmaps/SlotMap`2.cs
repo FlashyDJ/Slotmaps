6bbe39fe96fbd499525e1e0d65a7f5b705b5cdcc
FlashyDJ <77375978+FlashyDJ@users.noreply.github.com>
2024-02-01 21:09:24 +0800

    Merge SlotMap's nested classes and struct to one file

--- src/Slotmaps/SlotMap`2.cs
+++ src/Slotmaps/SlotMap`2.cs
@@ -10,7 +10,7 @@ namespace FlashyDJ.Slotmaps;
 /// <typeparam name="TKey">The struct type of keys that implement. <see cref="ISlotKey{TKey}"/>.</typeparam>
 /// <typeparam name="TValue">The type of values stored in the slot map.</typeparam>
 [DebuggerDisplay("Count = {Count}")]
-public partial class SlotMap<TKey, TValue> : IEnumerable<KeyValuePair<TKey, TValue>>, IEnumerable
+public class SlotMap<TKey, TValue> : IEnumerable<KeyValuePair<TKey, TValue>>, IEnumerable
     where TKey : struct, ISlotKey<TKey>
 {
     private const int DefaultCapacity = 4;
@@ -426,6 +426,35 @@ public partial class SlotMap<TKey, TValue> : IEnumerable<KeyValuePair<TKey, TVal
         return slot.Update(value);
     }
 
+    [DebuggerDisplay("{ToString()}")]
+    private struct Slot(TValue value, uint version)
+    {
+        public TValue Value = value;
+
+        public uint Version = version;
+
+        public int NextFree;
+
+        public readonly bool Occupied => Version % 2 > 0;
+
+        public TValue Clear(int nextFree)
+        {
+            var value = Value;
+            Value = default!;
+            NextFree = nextFree;
+            Version++;
+            return value;
+        }
+
+        public uint Update(TValue value)
+        {
+            Value = value;
+            return Version += Occupied ? 2u : 1u;
+        }
+
+        public override string ToString() => $"{Value}v{Version}";
+    }
+
     /// <summary>
     ///   Represents an enumerator for the <see cref="SlotMap{TKey, TValue}"/> collection.
     /// </summary>
@@ -474,6 +503,254 @@ public partial class SlotMap<TKey, TValue> : IEnumerable<KeyValuePair<TKey, TVal
         {
             _index = -1;
             _current = default;
+        }
+    }
+
+    /// <summary>
+    /// Represents a read only collection of keys associated with a value in the slot map.
+    /// </summary>
+    [DebuggerDisplay("Count = {Count}")]
+    public sealed class SlotKeyCollection : ICollection<TKey>, IReadOnlyCollection<TKey>
+    {
+        private readonly SlotMap<TKey, TValue> _slotMap;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SlotKeyCollection"/> class with a reference
+        /// to the parent slot map.
+        /// </summary>
+        /// <param name="slotMap">The slot map to associate with this key collection. Must not be null.</param>
+        /// <exception cref="ArgumentNullException">
+        ///   Thrown if <paramref name="slotMap"/> is null.
+        /// </exception>
+        public SlotKeyCollection(SlotMap<TKey, TValue> slotMap)
+        {
+            ArgumentNullException.ThrowIfNull(slotMap);
+            _slotMap = slotMap;
+        }
+
+        bool ICollection<TKey>.IsReadOnly => true;
+        void ICollection<TKey>.Add(TKey item) => throw new NotSupportedException();
+        void ICollection<TKey>.Clear() => throw new NotSupportedException();
+        bool ICollection<TKey>.Remove(TKey item) => throw new NotSupportedException();
+        IEnumerator IEnumerable.GetEnumerator() => new Enumerator(_slotMap);
+
+        /// <summary>Gets the number of keys in the collection.</summary>
+        public int Count => _slotMap.Count;
+
+        /// <summary>
+        ///   Determines whether the collection contains a specific key.
+        /// </summary>
+        /// <param name="key">The key to search for in the collection.</param>
+        /// <returns>
+        ///   <see langword="true"/> if the slot map contains the specified key; otherwise, <see langword="false"/>.
+        /// </returns>
+        public bool Contains(TKey key) => _slotMap.ContainsKey(key);
+
+        /// <summary>
+        ///   Copies the elements of the key collection to an array, starting at the specified index.
+        /// </summary>
+        /// <param name="array">The destination array where keys will be copied.</param>
+        /// <param name="index">The zero-based index in the array at which copying begins.</param>
+        /// <exception cref="ArgumentNullException">
+        ///   Thrown if <paramref name="array"/> is null.
+        /// </exception>
+        /// <exception cref="ArgumentOutOfRangeException">
+        ///   Thrown if <paramref name="index"/> is negative, greater than or equal to the length of
+        ///   <paramref name="array"/>, or if there are not enough elements in the slot map to fill the
+        ///   destination array starting at the specified index.
+        /// </exception>
+        public void CopyTo(TKey[] array, int index)
+        {
+            ArgumentNullException.ThrowIfNull(array);
+            ArgumentOutOfRangeException.ThrowIfNegative(index);
+            ArgumentOutOfRangeException.ThrowIfGreaterThan(index, array.Length);
+            ArgumentOutOfRangeException.ThrowIfLessThan(_slotMap.Count, array.Length - index);
+
+            for (int i = 0; i < _slotMap.Capacity; i++)
+            {
+                var slot = _slotMap._slots[i];
+
+                if (slot.Occupied)
+                    array[index++] = TKey.New(i, slot.Version);
+            }
+        }
+
+        /// <inheritdoc/>
+        public IEnumerator<TKey> GetEnumerator() => new Enumerator(_slotMap);
+
+        /// <summary>
+        ///   Represents an enumerator for the keys in the <see cref="SlotKeyCollection"/>.
+        /// </summary>
+        public struct Enumerator : IEnumerator<TKey>, IEnumerator
+        {
+            private readonly SlotMap<TKey, TValue> _slotMap;
+            private int _index;
+            private TKey _current;
+
+            internal Enumerator(SlotMap<TKey, TValue> slotMap)
+            {
+                _slotMap = slotMap;
+                _index = -1;
+                _current = default;
+            }
+
+            object IEnumerator.Current => Current;
+
+            /// <inheritdoc/>
+            public TKey Current => _current;
+
+            /// <inheritdoc/>
+            public readonly void Dispose() { }
+
+            /// <inheritdoc/>
+            public bool MoveNext()
+            {
+                while (_index < _slotMap.Capacity)
+                {
+                    while (++_index < _slotMap.Capacity)
+                    {
+                        var slot = _slotMap._slots[_index];
+
+                        if (!slot.Occupied)
+                            break;
+
+                        _current = TKey.New(_index, slot.Version);
+                        return true;
+                    }
+                }
+                return false;
+            }
+
+            /// <inheritdoc/>
+            public void Reset()
+            {
+                _index = -1;
+                _current = default;
+            }
+        }
+    }
+
+    /// <summary>
+    ///   Represents a readonly collection of values in a slot map.
+    /// </summary>
+    [DebuggerDisplay("Count = {Count}")]
+    public sealed class SlotValueCollection : ICollection<TValue>, IReadOnlyCollection<TValue>
+    {
+        private readonly SlotMap<TKey, TValue> _slotMap;
+
+        /// <summary>
+        ///   Initializes a new instance of the <see cref="SlotValueCollection"/> class with a reference to the
+        ///   parent slot map.
+        /// </summary>
+        /// <param name="slotMap">The slot map to associate with this value collection. Must not be null.</param>
+        /// <exception cref="ArgumentNullException">
+        ///   Thrown if <paramref name="slotMap"/> is null.
+        /// </exception>
+        public SlotValueCollection(SlotMap<TKey, TValue> slotMap)
+        {
+            ArgumentNullException.ThrowIfNull(slotMap);
+            _slotMap = slotMap;
+        }
+
+        bool ICollection<TValue>.IsReadOnly => true;
+        void ICollection<TValue>.Add(TValue item) => throw new NotSupportedException();
+        void ICollection<TValue>.Clear() => throw new NotSupportedException();
+        bool ICollection<TValue>.Remove(TValue item) => throw new NotSupportedException();
+        IEnumerator IEnumerable.GetEnumerator() => new Enumerator(_slotMap);
+
+        /// <summary>Gets the number of values in the collection.</summary>
+        public int Count => _slotMap.Count;
+
+        /// <summary>
+        ///   Determines whether the collection contains a specific value.
+        /// </summary>
+        /// <param name="value">The value to search for in the collection.</param>
+        /// <returns>
+        ///  <see langword="true"/> if the slot map contains the specified value; otherwise, <see langword="false"/>.
+        /// </returns>
+        public bool Contains(TValue value) => _slotMap.ContainsValue(value);
+
+        /// <summary>
+        ///   Copies the elements of the value collection to an array, starting at the specified index.
+        /// </summary>
+        /// <param name="array">The destination array where values will be copied.</param>
+        /// <param name="index">The zero-based index in the array at which copying begins.</param>
+        /// <exception cref="ArgumentNullException">
+        ///   Thrown if <paramref name="array"/> is null.
+        /// </exception>
+        /// <exception cref="ArgumentOutOfRangeException">
+        ///   Thrown if <paramref name="index"/> is negative, greater than or equal to the length of
+        ///   <paramref name="array"/>, or if there are not enough elements in the slot map to fill the
+        ///   destination array starting at the specified index.
+        /// </exception>
+        public void CopyTo(TValue[] array, int index)
+        {
+            ArgumentNullException.ThrowIfNull(array);
+            ArgumentOutOfRangeException.ThrowIfNegative(index);
+            ArgumentOutOfRangeException.ThrowIfGreaterThan(index, array.Length);
+            ArgumentOutOfRangeException.ThrowIfLessThan(Count, array.Length - index);
+
+            for (int i = 0; i < _slotMap.Capacity; i++)
+            {
+                var slot = _slotMap._slots[i];
+
+                if (slot.Occupied)
+                    array[index++] = slot.Value;
+            }
+        }
+
+        /// <inheritdoc/>
+        public IEnumerator<TValue> GetEnumerator() => new Enumerator(_slotMap);
+
+        /// <summary>
+        ///   Represents an enumerator for the values in the <see cref="SlotValueCollection"/>
+        /// </summary>
+        public struct Enumerator : IEnumerator<TValue>, IEnumerator
+        {
+            private readonly SlotMap<TKey, TValue> _slotMap;
+            private int _index;
+            private TValue _current;
+
+            internal Enumerator(SlotMap<TKey, TValue> slotMap)
+            {
+                _slotMap = slotMap;
+                _index = -1;
+                _current = default!;
+            }
+
+            object? IEnumerator.Current => Current;
+
+            /// <inheritdoc/>
+            public TValue Current => _current;
+
+            /// <inheritdoc/>
+            public void Dispose() { }
+
+            /// <inheritdoc/>
+            public bool MoveNext()
+            {
+                while (_index < _slotMap.Capacity)
+                {
+                    while (++_index < _slotMap.Capacity)
+                    {
+                        var slot = _slotMap._slots[_index];
+
+                        if (!slot.Occupied)
+                            break;
+
+                        _current = slot.Value;
+                        return true;
+                    }
+                }
+                return false;
+            }
+
+            /// <inheritdoc/>
+            public void Reset()
+            {
+                _index = -1;
+                _current = default!;
+            }
         }
     }
 }
