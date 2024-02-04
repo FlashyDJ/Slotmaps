761c82d22818a6a61e7bcba3b44a6393e995c7b7
FlashyDJ <77375978+FlashyDJ@users.noreply.github.com>
2024-02-04 18:09:37 +0800

    Add program

--- samples/RandMeldHeap/Program.cs
+++ samples/RandMeldHeap/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RandMeldHeap;
+
+Console.WriteLine("Randomized Meldable Heap Test");
+
+var rmh = new RandomizedMeldableHeap<int>();
+var theAnswer = rmh.Insert(-2);
+var big = rmh.Insert(999);
+
+for (int k = 9; k >= 0; k--)
+    rmh.Insert(k * k);
+
+rmh.UpdateKey(theAnswer, 42);
+rmh.RemoveKey(big);
+
+while (rmh.Count > 0)
+    Console.WriteLine(rmh.Pop());
+
+var rmh2 = new RandomizedMeldableHeap<int>();
+var theAnswer2 = rmh2.Insert(-2);
+var big2 = rmh2.Insert(999);
+
+foreach (var k in Enumerable.Range(0,10))
+    rmh2.Insert(k * k);
+
+rmh2.UpdateKey(theAnswer2, 42);
+rmh2.RemoveKey(big2);
+
+while (rmh2.Count > 0)
+    Console.WriteLine(rmh2.Pop());
+
+Console.ReadKey(true);
